abb54f814b37c5b259bef51e0e4ce4f0ef1663c7
Umerkhan6595 <umerkhanbuisness97@gmail.com>
2026-01-13 16:21:33 +0500

    Serve frontend build from backend

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
