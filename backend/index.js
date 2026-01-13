d868a9a465851f3580b9cb7b576691b0d31c4bd2
Umerkhan6595 <umerkhanbuisness97@gmail.com>
2026-01-13 16:17:07 +0500

    final project

--- backend/index.js
+++ backend/index.js
@@ -25,11 +25,13 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/service-requests', require('./routes/serviceRequestRoutes'));
 
 
-app.use(express.static(path.join(_dirname, '/frontend/dist')));
-app.get("/verify-email/:token", (req, res) => {
-    res.sendFile(path.join(_dirname, '/frontend/dist/index.html'));
-});
+const __dirname = path.resolve();
 
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
