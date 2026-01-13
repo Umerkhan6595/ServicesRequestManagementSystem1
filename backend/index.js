e1fd206340e73b67526d9314123d6659afe378a3
Umerkhan6595 <umerkhanbuisness97@gmail.com>
2026-01-13 16:43:42 +0500

    final project

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'https://servicesrequestmanagementsystem1.onrender.com/api',
+    origin: 'https://servicesrequestmanagementsystem1.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
